e441f1205c095b977ac83477091cbebc4b26d590
Amanda gabriela <amandagabriela056@gmail.com>
2025-05-23 11:05:39 -0300

    feat: atv-4 - Inclusão de Relacionamento N para N Empréstimo

--- atividade_4.1/database/seeders/DatabaseSeeder.php
+++ atividade_4.1/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             AuthorPublisherBookSeeder::class,
+            UserBorrowingSeeder::class, 
         ]);
     }
 }
