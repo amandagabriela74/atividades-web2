0648dc6c3abe9da3adc629191fb0fd3c6a2769fe
Amanda Gabriela <amandagabriela056@gmail.com>
2025-05-30 10:17:20 -0300

    feat: Atividade 5.2 -  Construção do Controller, Rotas e Views para a Entidade Publishers

--- atividade_5/routes/web.php
+++ atividade_5/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
 
 Route::get('/', function () {
     return view('home');
@@ -13,3 +14,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('categories', CategoryController::class);
 Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
