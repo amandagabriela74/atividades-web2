9070c76db7ed86f5001c6e2ffb87ad56c9875078
Amanda Gabriela <amandagabriela056@gmail.com>
2025-05-30 10:07:46 -0300

    Atividade Prática 5.2: Construção do Controller, Rotas e Views para a Entidade Author

--- atividade_5/routes/web.php
+++ atividade_5/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('home');
@@ -11,3 +12,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
